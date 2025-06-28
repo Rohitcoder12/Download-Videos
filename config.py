59ac0676a711ae42584ef9639d7701ba18a25fa2
Rohitcoder12 <rohitgandu91@gmail.com>
2025-06-28 23:09:19 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# --- Your Telegram Bot Token from @BotFather ---
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
-
-# --- The ID of your private channel (must be a number, not a string) ---
-# It should start with -100
-DUMB_CHANNEL_ID = -1001234567890
